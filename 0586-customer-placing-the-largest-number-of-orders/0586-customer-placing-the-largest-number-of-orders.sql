582e760ee1c15dbccf413d8e3c9c7e14c139cc9e
omgorle15 <omgorle5@gmail.com>
2026-08-14 11:21:58 +0530

    Time: 483 ms (64.06%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-SELECT customer_number FROM Orders GROUP BY customer_number ORDER BY COUNT(order_number) DESC  LIMIT 1;
+SELECT customer_number FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(order_number) DESC
+LIMIT 1;
